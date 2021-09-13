e92f8ed1d9f02e4af6dd247f1c751056d822a3b7
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-13 12:08:06 -0300

    Exercicio3 da revisão

---
+++ src/br/com/zup/Aula1/Exercicio3_Revisão.java
@@ -0,0 +1,52 @@
+package br.com.zup.Aula1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Exercicio3_Revisão {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String, String> prato = new HashMap<String, String>();
+
+        int menu = 1;
+        int escolhaMenu = 0;
+
+        while (menu != 4){
+            System.out.println("Digite 1: Para cadastrar um prato");
+            System.out.println("Digite 2: Para exibir a lista de pratos");
+            System.out.println("Digite 3: Para excluir um prato");
+            System.out.println("Digite 4: Para finalizar o programa");
+            escolhaMenu = leitor.nextInt();
+            leitor.nextLine();
+            if (escolhaMenu == 1){
+                System.out.println("Por favor, digite o nome do prato: ");
+                String nomeDoPrato = leitor.nextLine();
+                System.out.println("Por favor, digite os ingredientes do prato(use virgula para separar os ingredientes)");
+                String ingredientes = leitor.nextLine();
+
+                prato.put(nomeDoPrato, ingredientes);
+            }
+            if (escolhaMenu == 2){
+                for (String listaDePratos : prato.keySet()){
+                    System.out.println("Nome do Prato: " +listaDePratos + ", Ingredientes: " +prato.get(listaDePratos));
+                }
+
+            }
+            if (escolhaMenu == 3){
+                System.out.println("Por favor, digite o nome do prato");
+                String pratoExcluido = leitor.nextLine();
+                if (prato.containsKey(pratoExcluido)){
+                    System.out.println("Prato excluido com sucesso.");
+                    prato.remove(pratoExcluido);
+                }else{
+                    System.out.println("Prato não cadastrado.");
+                }
+            }
+            if (escolhaMenu == 4){
+                menu = 4;
+                System.out.println("Programa finalizado.");
+            }
+        }
+    }
+}
