6c2acf8ca1897ae65fae366b1b70e5cb361059b2
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-13 10:40:30 -0300

    Primeiro commit

---
+++ src/br/com/zup/Aula1/Exercicio1_Revisão.java
@@ -0,0 +1,12 @@
+package br.com.zup.Aula1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Exercicio1_Revisão {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String, Double> produto = new HashMap<String, Double>();
+    }
+}
